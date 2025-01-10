3c1722bf9cb51240dfb85a7a3d6bb990290a4101
Yuvraj Rasal <yuvrajrasal13@gmail.com>
2025-01-10 15:49:15 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,29 @@
-const accountId = 1441
-let accountEmail = "yuvrajrasal13@gmail.com"
-var passs= "12345"
-accountCity = "Satara"
+// const accountId = 1441
+// let accountEmail = "yuvrajrasal13@gmail.com"
+// var passs= "12345"
+// accountCity = "Satara"
+
+// console.log(accountId);
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
 
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
